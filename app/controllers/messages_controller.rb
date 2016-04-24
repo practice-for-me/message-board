2bc4d41cd28081b8df6966def9d5272e46690073
suemitu <suemiken@icloud.com>
2016-04-24 07:32:46 +0000

    controller def_create create

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,15 @@
 class MessagesController < ApplicationController
   def index
+    @message = Message.new
   end
+  
+  def create
+    @message = Message.new(message_params)
+    @message.save
+    redirect_to root_path ,notice:'メッセージを保存しました'
+  end
+  
+  private
+  def message_params
+    params.require(:message).permit8:name,:body)
 end
--- db/migrate/20160423124645_create_messages.rb
+++ db/migrate/20160423124645_create_messages.rb
@@ -1,9 +1,11 @@
 class CreateMessages < ActiveRecord::Migration
   def change
+    #messagesテーブルの作成
     create_table :messages do |t|
       t.string :name
       t.string :body
-
+      
+      #テーブルの更新と作成時間をカラムとして記録
       t.timestamps null: false
     end
   end
