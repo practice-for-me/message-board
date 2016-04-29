6c63a00a35c8f2199b0712b6f8b919b3ca714286
suemitu <suemiken@icloud.com>
2016-04-29 00:57:31 +0000

    message.rb update

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,6 @@ class Message < ActiveRecord::Base
     validates :name ,length:{maximum:20}, presence:true
     #内容は必須入力かつ２文字以上３０文字以内
     validates :body ,length: {minimum:2, maximum:30}, presence:true
+    #年れは0以上の数字だけ
+    validates :age ,length:{minimum:0}
 end
